8c07f8afb88543db4fb9360e848b0330e63d0fc0
shubh72010 <108581630+shubh72010@users.noreply.github.com>
2025-07-21 22:04:37 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAmD9lC7CGAn4zUgM59IAXXmVam3N8Vr1o",
+  authDomain: "zbots-90001.firebaseapp.com",
+  projectId: "zbots-90001",
+  storageBucket: "zbots-90001.appspot.com",
+  messagingSenderId: "517190005064",
+  appId: "1:517190005064:web:e849a5ec88a84b752d837d",
+  measurementId: "G-JXG10W9DN7",
+};
+
+module.exports = { firebaseConfig };
